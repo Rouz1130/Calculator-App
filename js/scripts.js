240658ca0015c5d8358b07ea7dbbfd900af8f5b0
rouz majlessi <rmajlessi@gmail.com>
2017-03-12 01:25:57 -0800

    add two user inputs for two numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 var add = function(number1 , number2) {
   return number1 + number2;
 };
+// built in prompt function takes string as argument , returns string for user input.
+// prompt collects info unlike alert.
+var number1 = prompt("Enter a Number");
+var number2 = prompt("Enter another Number");
 
-alert(add(10,5));
+alert(add(number1, number2));
