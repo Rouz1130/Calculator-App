5b5bddbb340788d161807da9d56c87a6872849e6
rouz majlessi <rmajlessi@gmail.com>
2017-03-12 01:37:19 -0800

    create more files, add html , script tags, and add variable result and alert to show result

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ var add = function(number1 , number2) {
 };
 // built in prompt function takes string as argument , returns string for user input.
 // prompt collects info unlike alert.
-var number1 = prompt("Enter a Number");
-var number2 = prompt("Enter another Number");
+var number1 = parseInt(prompt("Enter a Number"));
+// need to add parseInt or else we would get two strings returned concated with one another.
+var number2 = parseInt(prompt("Enter another Number"));
+var result = add(number1, number2);
 
-alert(add(number1, number2));
+alert(result);
